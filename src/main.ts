6d5eb313d3f7eda28e68d491d96ef56e3b3cc3fa
huangxinpeng <gp.3098@qq.com>
2023-10-04 23:35:23 +0800

    重点在于入口点使用注入

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+import 'reflect-metadata';
 import { Application } from '@src/Application.service';
 import { SystemController } from '@src/controllers/System.controller';
 import { SystemService } from '@src/services/system.service';
+import { container } from '@src/container';
 
 // const systemService = new SystemService();
-const controllers = [new SystemController()];
+// const controllers = [new SystemController()];
+const controllers = [container.get(SystemController)];
 export const app = new Application({ controllers });
 async function main() {
   app.listen(8888);
